4ed837768d046f0aa1af5fe3e065508efb09f723
xmaster3213 <emailperlascuolamia@gmail.com>
2022-05-09 16:36:19 +0200

    Added page Default for common functionalities

---
+++ src/pages/Default.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Default extends React.Component {
+
+_request;
+state = {
+  data: null,
+};
+
+  constructor(props) {
+    super(props);
+    this._request = require('../functions/functionalities');
+  }
+
+  render() {
+    if (this.state.data === null) {
+      return <h2>Loading...</h2>
+    } else {
+      return this._renderData();
+    }
+  }
+
+  // @Abstract
+  _renderData() {
+    throw new Error('This method is not yet implemented');
+  }
+
+  
+
+
+}
+
+export default Default;
